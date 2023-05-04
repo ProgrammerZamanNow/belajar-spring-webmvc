62b1e4c3616db73e71357e482f6e9cb8cfa1489e
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-04 23:13:52 +0700

    Response Body

--- src/main/java/programmerzamannow/webmvc/controller/DateController.java
+++ src/main/java/programmerzamannow/webmvc/controller/DateController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -15,8 +16,8 @@ public class DateController {
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 
     @GetMapping(path = "/date")
-    public void getDate(@RequestParam(name = "date") Date date,
-                        HttpServletResponse response) throws IOException {
-        response.getWriter().println("Date : " + dateFormat.format(date));
+    @ResponseBody
+    public String getDate(@RequestParam(name = "date") Date date) throws IOException {
+        return "Date : " + dateFormat.format(date);
     }
 }
