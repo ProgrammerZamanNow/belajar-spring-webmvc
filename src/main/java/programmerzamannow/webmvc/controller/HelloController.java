a6dc6b5f44d94b554b69e0f7e762713d3b1121f9
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-04 08:43:11 +0700

    Mock MVC

--- src/main/java/programmerzamannow/webmvc/controller/HelloController.java
+++ src/main/java/programmerzamannow/webmvc/controller/HelloController.java
@@ -1,17 +1,23 @@
 package programmerzamannow.webmvc.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Controller
 public class HelloController {
 
     @RequestMapping(path = "/hello")
-    public void helloWorld(HttpServletResponse response) throws IOException {
-        response.getWriter().println("Hello World");
+    public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name");
+        if (Objects.isNull(name)) {
+            name = "Guest";
+        }
+        response.getWriter().println("Hello " + name);
     }
 
 }
---
+++ src/test/java/programmerzamannow/webmvc/controller/HelloControllerTest.java
@@ -0,0 +1,42 @@
+package programmerzamannow.webmvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void helloGuest() throws Exception {
+        mockMvc.perform(
+                get("/hello")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hello Guest"))
+        );
+    }
+
+    @Test
+    void helloName() throws Exception {
+        mockMvc.perform(
+                get("/hello").queryParam("name", "Eko")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hello Eko"))
+        );
+    }
+}
