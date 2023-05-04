6bbad41badd645881f49a4f4df6be852ae2fa22c
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-04 08:18:12 +0700

    belajar spring web mvc

---
+++ src/main/java/programmerzamannow/webmvc/BelajarSpringWebMvcApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringWebMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmerzamannow/webmvc/BelajarSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.webmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
