0bfd7ea0f764d3326466996e81ee7ed018ae3820
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-04 08:25:55 +0700

    Request Mapping

--- src/main/java/programmerzamannow/webmvc/controller/HelloController.java
+++ src/main/java/programmerzamannow/webmvc/controller/HelloController.java
@@ -1,8 +1,17 @@
 package programmerzamannow.webmvc.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 public class HelloController {
+
+    @RequestMapping(path = "/hello")
+    public void helloWorld(HttpServletResponse response) throws IOException {
+        response.getWriter().println("Hello World");
+    }
 
 }
