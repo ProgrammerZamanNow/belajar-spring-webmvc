3878afb7f02ab93a2d95947d9953088aa3584fa9
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-05 15:33:50 +0700

    Response Content Type

--- src/main/java/programmerzamannow/webmvc/controller/FormController.java
+++ src/main/java/programmerzamannow/webmvc/controller/FormController.java
@@ -9,9 +9,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class FormController {
 
-    @PostMapping(path = "/form/hello", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @PostMapping(
+            path = "/form/hello",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.TEXT_HTML_VALUE
+    )
     @ResponseBody
     public String hello(@RequestParam(name = "name") String name) {
-        return "Hello " + name;
+        return """
+                <html>
+                <body>
+                <h1>Hello $name</h1>
+                </body>
+                </html>
+                """.replace("$name", name);
     }
 }
--- src/test/java/programmerzamannow/webmvc/controller/FormControllerTest.java
+++ src/test/java/programmerzamannow/webmvc/controller/FormControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,6 +30,7 @@ class FormControllerTest {
                         .param("name", "Eko")
         ).andExpectAll(
                 status().isOk(),
+                header().string(HttpHeaders.CONTENT_TYPE, Matchers.containsString(MediaType.TEXT_HTML_VALUE)),
                 content().string(Matchers.containsString("Hello Eko"))
         );
     }
