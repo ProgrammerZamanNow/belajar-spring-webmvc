40d588337b9be2d7772d887b9f773c232dcde576
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-06 10:32:35 +0700

    MVC Config

---
+++ src/main/java/programmerzamannow/webmvc/MyWebConfig.java
@@ -0,0 +1,8 @@
+package programmerzamannow.webmvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebConfig implements WebMvcConfigurer {
+}
