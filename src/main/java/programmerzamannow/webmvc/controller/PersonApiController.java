22b273e138185cea38b0f450355a975c4acaf037
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-06 09:44:30 +0700

    Json

---
+++ src/main/java/programmerzamannow/webmvc/controller/PersonApiController.java
@@ -0,0 +1,22 @@
+package programmerzamannow.webmvc.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import programmerzamannow.webmvc.model.CreatePersonRequest;
+
+@Controller
+public class PersonApiController {
+
+    @PostMapping(
+            path = "/api/person",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public CreatePersonRequest createPerson(@RequestBody CreatePersonRequest request){
+        return request;
+    }
+}
---
+++ src/test/java/programmerzamannow/webmvc/controller/PersonApiControllerTest.java
@@ -0,0 +1,57 @@
+package programmerzamannow.webmvc.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import programmerzamannow.webmvc.model.CreatePersonRequest;
+import programmerzamannow.webmvc.model.CreateSocialMediaRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PersonApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void createPerson() throws Exception{
+        CreatePersonRequest request = new CreatePersonRequest();
+        request.setFirstName("Eko");
+        request.setMiddleName("Kurniawan");
+        request.setLastName("Khannedy");
+        request.setEmail("eko@example.com");
+        request.setPhone("08213123");
+        request.setHobbies(List.of("Coding", "Reading", "Gaming"));
+        request.setSocialMedias(new ArrayList<>());
+        request.getSocialMedias().add(new CreateSocialMediaRequest("Facebook", "facebook.com/ProgrammerZamanNow"));
+        request.getSocialMedias().add(new CreateSocialMediaRequest("Instagram", "instagram.com/ProgrammerZamanNow"));
+
+        String jsonRequest = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                post("/api/person")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest)
+        ).andExpectAll(
+                status().isOk(),
+                content().json(jsonRequest)
+        );
+    }
+}
